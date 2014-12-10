e6b77bc6d37f13f31a829f4e5139a5b467c829b8
Ben Stott <bgbnbigben@gmail.com>
2014-12-10 20:17:04 +1100

    Generic statistics class.

---
+++ Statistics.java
@@ -0,0 +1,18 @@
+package games.ttd;
+
+public class Statistics {
+
+	public int tracksBought;
+	public int moneyMade;
+
+	public Statistics() {
+        tracksBought = 0;
+        moneyMade = 0;
+	}
+
+	public Statistics(Statistics s) {
+        tracksBought = s.tracksBought;
+        moneyMade = s.moneyMade;
+	}
+
+}
