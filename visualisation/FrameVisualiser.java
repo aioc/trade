61e0aec70dabc72f0a17ca26f5cc956c387ee0cd
Luke Harrison <dahlukeh@gmail.com>
2014-12-18 21:28:59 +1100

    Fixed up some formatting and removed stuff

--- visualisation/FrameVisualiser.java
+++ visualisation/FrameVisualiser.java
@@ -2,7 +2,6 @@ package games.ttd.visualisation;
 
 import games.ttd.Consumer;
 import games.ttd.Producer;
-import games.ttd.Statistics;
 import games.ttd.Track;
 
 import java.awt.BasicStroke;
@@ -33,17 +32,15 @@ public class FrameVisualiser implements FrameVisualisationHandler<VisualGameStat
 	Rectangle paintBox;
 	Color statBoxColour = null;
 	Color textColour = null;
-	
+
 	@Override
 	public void generateBackground(VisualGameState s, int sWidth, int sHeight, Graphics2D g) {
-		paintBox = new Rectangle(BORDER_SIZE, BORDER_SIZE, sWidth - (2 * BORDER_SIZE), sHeight
-				- (2 * BORDER_SIZE));
+		paintBox = new Rectangle(BORDER_SIZE, BORDER_SIZE, sWidth - (2 * BORDER_SIZE), sHeight - (2 * BORDER_SIZE));
 		sizeBoard = Math.min(paintBox.height - (2 * BORDER_SIZE), (3 * (paintBox.width - (2 * BORDER_SIZE))) / 5);
 		if (sizeBoard < 0) {
 			sizeBoard = 0;
 		}
-		int rectWidth = (17 * (paintBox.width - (2 * BORDER_SIZE))) / 20,
-			rectHeight = paintBox.height + BORDER_SIZE;
+		int rectWidth = (17 * (paintBox.width - (2 * BORDER_SIZE))) / 20, rectHeight = paintBox.height + BORDER_SIZE;
 		boardBox = new Rectangle(paintBox.x + BORDER_SIZE, paintBox.y + BORDER_SIZE, rectWidth, rectHeight);
 		sizeRectWidth = rectWidth / s.boardSize;
 		sizeRectHeight = rectHeight / s.boardSize;
@@ -55,28 +52,19 @@ public class FrameVisualiser implements FrameVisualisationHandler<VisualGameStat
 		g.setColor(Color.LIGHT_GRAY.brighter());
 		for (int i = 0; i < s.boardSize; i++) {
 			for (int j = 0; j < s.boardSize; j++) {
-				g.fillRect(
-					boardBox.x + (j * sizeRectWidth),
-					boardBox.y + (i * sizeRectHeight),
-					sizeRectWidth - borderSquareSize,
-					sizeRectHeight - borderSquareSize);
+				g.fillRect(boardBox.x + (j * sizeRectWidth), boardBox.y + (i * sizeRectHeight), sizeRectWidth
+						- borderSquareSize, sizeRectHeight - borderSquareSize);
 			}
 		}
 		for (Producer p : s.producers) {
 			g.setColor(p.getColour().darker().darker().darker());
-			g.fillRect(
-					boardBox.x + (p.c * sizeRectWidth),
-					boardBox.y + (p.r * sizeRectHeight),
-					sizeRectWidth - borderSquareSize,
-					sizeRectHeight - borderSquareSize);
+			g.fillRect(boardBox.x + (p.c * sizeRectWidth), boardBox.y + (p.r * sizeRectHeight), sizeRectWidth
+					- borderSquareSize, sizeRectHeight - borderSquareSize);
 		}
 		for (Consumer c : s.consumers) {
 			g.setColor(c.getColour().brighter().brighter().brighter().brighter());
-			g.fillRect(
-					boardBox.x + (c.c * sizeRectWidth),
-					boardBox.y + (c.r * sizeRectHeight),
-					sizeRectWidth - borderSquareSize,
-					sizeRectHeight - borderSquareSize);
+			g.fillRect(boardBox.x + (c.c * sizeRectWidth), boardBox.y + (c.r * sizeRectHeight), sizeRectWidth
+					- borderSquareSize, sizeRectHeight - borderSquareSize);
 		}
 	}
 
@@ -104,19 +92,20 @@ public class FrameVisualiser implements FrameVisualisationHandler<VisualGameStat
 	@Override
 	public void eventCreated(VisualGameEvent e) {
 		if (e instanceof TTDGainedMoneyEvent) {
-			TTDGainedMoneyEvent te = (TTDGainedMoneyEvent)e;
+			TTDGainedMoneyEvent te = (TTDGainedMoneyEvent) e;
 			te.setTotalFrames(TRACKFRAMES);
 		} else if (e instanceof TTDLostMoneyEvent) {
-			TTDLostMoneyEvent te = (TTDLostMoneyEvent)e;
+			TTDLostMoneyEvent te = (TTDLostMoneyEvent) e;
 			te.setTotalFrames(TRACKFRAMES);
 		} else {
-			TTDGameEvent te = (TTDGameEvent)e;
+			TTDGameEvent te = (TTDGameEvent) e;
 			te.setTotalFrames(TRACKFRAMES);
 		}
 	}
 
 	@Override
-	public void animateEvents(VisualGameState currentState, List<VisualGameEvent> events, int sWidth, int sHeight, Graphics2D g) {
+	public void animateEvents(VisualGameState currentState, List<VisualGameEvent> events, int sWidth, int sHeight,
+			Graphics2D g) {
 		Rectangle textBox = new Rectangle(boardBox.x + boardBox.width + BORDER_SIZE, paintBox.y + BORDER_SIZE,
 				paintBox.width - boardBox.width - 2 * BORDER_SIZE, paintBox.height - BORDER_SIZE);
 		if (statBoxColour == null)
@@ -128,15 +117,15 @@ public class FrameVisualiser implements FrameVisualisationHandler<VisualGameStat
 			if (!(events.get(i) instanceof TTDGameEvent)) {
 				continue;
 			}
-			TTDGameEvent te = (TTDGameEvent)events.get(i);
-			for (Track track: te.tracks) {
+			TTDGameEvent te = (TTDGameEvent) events.get(i);
+			for (Track track : te.tracks) {
 				currentState.stats[te.player].tracksBought++;
 				drawTrack(g, currentState.colours[te.player], track, te.player, currentState.names.length);
 			}
 		}
-		
+
 	}
-	
+
 	private void drawTrack(Graphics2D g, Color colour, Track track, int pid, int numPlayers) {
 		g.setColor(colour);
 		int playerDotW = sizeRectWidth * (pid + 1) / (numPlayers + 1);
@@ -146,39 +135,34 @@ public class FrameVisualiser implements FrameVisualisationHandler<VisualGameStat
 		// ith of these dots.
 		if (track.d == 1 || track.d == 3) {
 			// d == 1 should be right, d == 3 should be left
-			g.drawLine(boardBox.x + track.c*sizeRectWidth + playerDotW,
-						boardBox.y + track.r*sizeRectHeight + playerDotH,
-						boardBox.x + track.c*sizeRectWidth + playerDotW - (sizeRectWidth*(track.d - 2)),
-						boardBox.y + track.r*sizeRectHeight + playerDotH
-					);
+			g.drawLine(boardBox.x + track.c * sizeRectWidth + playerDotW, boardBox.y + track.r * sizeRectHeight
+					+ playerDotH, boardBox.x + track.c * sizeRectWidth + playerDotW - (sizeRectWidth * (track.d - 2)),
+					boardBox.y + track.r * sizeRectHeight + playerDotH);
 		} else {
 			// d == 0 should be up, d == 2 should be down.
-			g.drawLine(boardBox.x + track.c*sizeRectWidth + playerDotW,
-						boardBox.y + track.r*sizeRectHeight + playerDotH,
-						boardBox.x + track.c*sizeRectWidth + playerDotW,
-						boardBox.y + track.r*sizeRectHeight + playerDotH + (sizeRectHeight*(track.d - 1))
-					);
+			g.drawLine(boardBox.x + track.c * sizeRectWidth + playerDotW, boardBox.y + track.r * sizeRectHeight
+					+ playerDotH, boardBox.x + track.c * sizeRectWidth + playerDotW, boardBox.y + track.r
+					* sizeRectHeight + playerDotH + (sizeRectHeight * (track.d - 1)));
 		}
 	}
 
 	private void drawStatBoxes(Graphics2D g, VisualGameState state, Rectangle textBox) {
 		final int numPlayers = state.names.length;
-		
+
 		Rectangle topBox = new Rectangle(textBox.x, textBox.y, textBox.width, textBox.height / 12);
 		Rectangle middleBox = new Rectangle(textBox.x, topBox.y + topBox.height, textBox.width,
 				(4 * textBox.height) / 6);
-		
+
 		int squareSize = (int) Math.ceil(Math.sqrt(numPlayers));
 		int squareWidth = middleBox.width;
 		int squareHeight = middleBox.height / squareSize;
 		int strokeSize = (Math.min(squareWidth, squareHeight) / 80) + 1;
 		squareWidth -= 2 * strokeSize;
 		squareHeight -= 2 * strokeSize;
-	
-		
+
 		for (int i = 0; i < numPlayers; i++) {
-			Rectangle playerBox = new Rectangle(middleBox.x + strokeSize,
-					middleBox.y + (i * (squareHeight + 2 * strokeSize)) + strokeSize, squareWidth, squareHeight);
+			Rectangle playerBox = new Rectangle(middleBox.x + strokeSize, middleBox.y
+					+ (i * (squareHeight + 2 * strokeSize)) + strokeSize, squareWidth, squareHeight);
 			Color pColour = state.colours[i];
 			Color nameColour = Color.WHITE;
 			while (textColour == null || state.colourDistance(textColour, statBoxColour) < 200) {
@@ -196,8 +180,10 @@ public class FrameVisualiser implements FrameVisualisationHandler<VisualGameStat
 			g.fillRect(nameBox.x, nameBox.y, nameBox.width, nameBox.height);
 			g.setFont(f);
 			g.setColor(nameColour);
-			g.drawString(state.names[i], nameBox.x + (nameBox.width - (int)fR.getWidth()) / 2, (nameBox.y + nameBox.height) - (8 * (int) (nameBox.height - fR.getHeight())) / 10);
-			Rectangle playerBoxInterior = new Rectangle(playerBox.x + strokeSize, playerBox.y + strokeSize, playerBox.width - 2*strokeSize, playerBox.height - 2*strokeSize);
+			g.drawString(state.names[i], nameBox.x + (nameBox.width - (int) fR.getWidth()) / 2,
+					(nameBox.y + nameBox.height) - (8 * (int) (nameBox.height - fR.getHeight())) / 10);
+			Rectangle playerBoxInterior = new Rectangle(playerBox.x + strokeSize, playerBox.y + strokeSize,
+					playerBox.width - 2 * strokeSize, playerBox.height - 2 * strokeSize);
 			g.setColor(textColour);
 			String s = "Money: $" + state.money[i];
 			f = getLargestFittingFont(g.getFont(), playerBoxInterior, g, s, 24);
@@ -205,13 +191,14 @@ public class FrameVisualiser implements FrameVisualisationHandler<VisualGameStat
 			fm = g.getFontMetrics(f);
 			fR = fm.getStringBounds(s, g);
 			g.drawString(s, playerBoxInterior.x, playerBoxInterior.y + (2 * playerBoxInterior.height) / 5);
-			
+
 			s = "Tracks bought: " + state.stats[i].tracksBought;
 			f = getLargestFittingFont(g.getFont(), playerBoxInterior, g, s, 24);
 			g.setFont(f);
-			//fm = g.getFontMetrics(f);
-			//fR = fm.getStringBounds(s, g);
-			g.drawString(s, playerBoxInterior.x, playerBoxInterior.y + (2 * playerBoxInterior.height) / 5 + (int)fR.getHeight() + 10);
+			// fm = g.getFontMetrics(f);
+			// fR = fm.getStringBounds(s, g);
+			g.drawString(s, playerBoxInterior.x,
+					playerBoxInterior.y + (2 * playerBoxInterior.height) / 5 + (int) fR.getHeight() + 10);
 		}
 	}
 
@@ -231,18 +218,18 @@ public class FrameVisualiser implements FrameVisualisationHandler<VisualGameStat
 		}
 		return f.deriveFont(minSize);
 	}
-	
+
 	@Override
 	public void eventEnded(VisualGameEvent e, VisualGameState state) {
 		if (e instanceof TTDGainedMoneyEvent) {
-			TTDGainedMoneyEvent te = (TTDGainedMoneyEvent)e;
-			state.money[te.player] += te.amountGained; 
+			TTDGainedMoneyEvent te = (TTDGainedMoneyEvent) e;
+			state.money[te.player] += te.amountGained;
 		} else if (e instanceof TTDLostMoneyEvent) {
-			TTDLostMoneyEvent te = (TTDLostMoneyEvent)e;
+			TTDLostMoneyEvent te = (TTDLostMoneyEvent) e;
 			state.money[te.player] -= te.amountLost;
 		} else {
-			TTDGameEvent te = (TTDGameEvent)e;
-			for (Track t : te.tracks){ 
+			TTDGameEvent te = (TTDGameEvent) e;
+			for (Track t : te.tracks) {
 				state.trackPlaced(t.r, t.c, te.player, t.d);
 			}
 		}
