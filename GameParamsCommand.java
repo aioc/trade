258de58693e06f7dd9ae65c80a483f919d263c1a
Luke Harrison <dahlukeh@gmail.com>
2014-12-19 14:44:26 +1100

    Added ability to change params

---
+++ GameParamsCommand.java
@@ -0,0 +1,58 @@
+package games.trade;
+
+import java.io.PrintStream;
+
+import core.Director;
+import core.commander.commands.Command;
+
+public class GameParamsCommand implements Command {
+
+	private GameFactory f;
+	public GameParamsCommand(GameFactory f) {
+		this.f = f;
+	}
+	
+	@Override
+	public void execute(Director reportTo, PrintStream out, String[] args) {
+		boolean badArgs = false;
+		int boardSize = 0;
+		int numProd = 0;
+		int numCons = 0;
+		int numTypes = 0;
+		int startMon = 0;
+		if (args.length != 5) {
+			badArgs = true;
+		} else {
+			try {
+				boardSize = Integer.parseInt(args[0]);
+				numProd = Integer.parseInt(args[1]);
+				numCons = Integer.parseInt(args[2]);
+				numTypes = Integer.parseInt(args[3]);
+				startMon = Integer.parseInt(args[4]);
+			} catch (NumberFormatException e) {
+				badArgs = true;
+			}
+		}
+		
+		if (badArgs) {
+			out.println("Usage: PARAMS boardSize, numProd, numCons, numTypes, startingMoney");
+		} else {
+			f.boardSize = boardSize;
+			f.initialMoney = startMon;
+			f.numConsumers = numCons;
+			f.numProducers = numProd;
+			f.numTypes = numTypes;
+		}
+	}
+
+	@Override
+	public String shortHelpString() {
+		return "Change the params of the games.\nIn order of boardSize, numProd, numCons, numTypes, startingMoney";
+	}
+
+	@Override
+	public String detailedHelpString() {
+		return null;
+	}
+
+}
--- TradeMain.java
+++ TradeMain.java
@@ -9,8 +9,11 @@ public class TradeMain {
 		Config config = new Config();
 		config.parseArgs(args);
 		config.port = 12317;
+		
+		
 		GameFactory f = new GameFactory();
+		config.gameCommands.put("PARAMS", new GameParamsCommand(f));
 		new Director(new PlayerFactory(), f).run(config);
 	}
-
+	
 }
